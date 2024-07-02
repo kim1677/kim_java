1d05b25af1d85bfb99baa35f9ab6d73df4d6bd59
kim1677 <rudwpww@naver.com>
2024-07-02 10:07:18 +0900

    123

--- java_module/src/java_0702/classBasic.java
+++ java_module/src/java_0702/classBasic.java
@@ -3,8 +3,16 @@ package java_0702;
 public class classBasic {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 클래스 이름은 대문자로 설정해야 함
+		
 
 	}
 
+}
+
+// 클래스는 사용자 정의 대에터 타입 이라고 한다.
+class Member{		// Member 라는 이름의 클래스 정의
+	String name;	// 이름
+	int age;		// 나이
+	
 }
