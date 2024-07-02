89d21d39f1646b1fa1808b08c5d286ed761e2742
kim1677 <rudwpww@naver.com>
2024-07-02 17:30:53 +0900

    123

---
+++ java_module/src/java0702_2/javaMethod.java
@@ -0,0 +1,60 @@
+package java0702_2;
+
+public class javaMethod {
+
+	public static void main(String[] args) {
+		// 클래스 메서드에서는 오직 클래스 변수나 클래스 메서드만
+		// 실행 - 메모리에 로드(적재)되는 때가 인스턴스보다 먼저 이루어지기 때문에 인스턴스 변수나 인스턴스 메서드는 안된다. 
+		int num = 300;
+		output(num );
+		
+	}
+	
+	static void output( int a ) {				// num 매개변수가 a에 저장됨
+		System.out.println(a);
+		System.out.println("나는 메서드 입니다. ");
+	}
+	
+
+}
+
+class a{
+	int num = 10;
+	
+	void out() {								// 클래스 메서드
+		System.out.println(num);
+	}
+}
+
+// main 메서드 → JVM
+
+
+
+
+// 자바 메서드
+// 반환타입 메서드이름(매개변수){	}
+
+// 반환타입 - int, short, float, double, char, boolean, void, String
+// 메서드의 return 데이터 타입
+// return 10; → 반환타입 int, short, long
+// return  income; → income 변수의 타입을 반환타입으로
+
+// void 타입은 반환값이 엾다라는 의미
+
+/*
+
+ 	990101 - 1234567
+	주민등록번호로 남자인지 여자인지 구별
+	String gender( String jumin ){
+	
+		jumin.subString( 7, 8 );
+		if( gen.equals("1") || gen.equals("3") ){
+			return "남자";
+		}else if( gen.equals("2") || gen.equals("4") ){
+			return "여자";
+		}
+		return "혼동";
+	}
+	
+*/
+		
--- java_module/src/java_0702/classBasic.java
+++ java_module/src/java_0702/classBasic.java
@@ -3,16 +3,104 @@ package java_0702;
 public class classBasic {
 
 	public static void main(String[] args) {
-		// 클래스 이름은 대문자로 설정해야 함
 		
-
+		// 클래스 이름은 대문자로 설정해야 함
+		// 정의된 클래스 타입의 공간을 생성 한다.
+		// 생성된 공간에 데이터를 저장한다.
+		// 클래스 타입으로 생성된 공간을 객체 또는 인스턴스라고 한다.
+		
+		// 클래스 인스턴스 생성 - 생성자 메서드
+		// new 연산자를 통해서 생성하는 변수는 참조변수
+		Member member1 = new Member();
+		Member member2 = new Member();
+		
+		System.out.println( member1 );
+		
+		member1.name = "이순신";
+		member1.age = 23;
+		
+		System.out.println( member1.name );
+		System.out.println( member1.age );
+		
+		member2.name = "김유신";
+		member2.age = 34;
+		
+		System.out.println( member2.name );
+		System.out.println( member2.age );
+		
+		
+		// java - 495p, 메모리최적화기법 - 893p
+		Book book1 = new Book();
+		Book book2 = new Book();
+		
+		System.out.println( book1 );
+		
+		book1.title = "java";
+		book1.page = 495;
+		
+		book2.title = "메모리최적화기법";
+		book2.page = 893;
+		
+		System.out.println( book1.title );
+		System.out.println( book1.page );
+		
+		System.out.println( book2.title );
+		System.out.println( book2.page );
+		
+		
+		
+		// 스마트폰 제품명, 운영체제, 메모리(ram)크기에 대해
+		// 저장할 수 있는 클래스를 정의하고 인스턴스 생성하여
+		// 값 저장, 출력을하세요(스마트폰 3개)
+		
+		Phone samsung = new Phone();
+		Phone apple = new Phone();		
+		
+		samsung.name = "갤럭시";
+		samsung.OS = "안드로이드";
+		samsung.ram = 16;
+		
+		System.out.println( samsung.name );	
+		System.out.println( samsung.OS );	
+		System.out.println( samsung.ram );
+		
+				
+		apple.name = "아이폰";
+		apple.OS = "IOS";
+		apple.ram = 8;
+		
+		System.out.println( apple.name );
+		System.out.println( apple.OS );
+		System.out.println( apple.ram );
+		
 	}
 
+	
 }
+
 
 // 클래스는 사용자 정의 대에터 타입 이라고 한다.
 class Member{		// Member 라는 이름의 클래스 정의
-	String name;	// 이름
-	int age;		// 나이
-	
+String name;	// 이름
+int age;		// 나이
+Member(){
+		
+	}
 }
+
+
+//책 제목과 총페이지수를 저장하기위한 클래스 정의
+class Book{
+	String title;
+	int page;
+}
+
+
+// 스마트폰 운영체제, 메모리 크기
+class Phone{
+	String name;
+	String OS;
+	int ram;
+}
+
+
