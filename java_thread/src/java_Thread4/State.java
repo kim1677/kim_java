dba7f898ad77f5af55548e29fbf3d6d105b95f96
kim1677 <rudwpww@naver.com>
2024-07-19 09:19:06 +0900

    123

---
+++ java_thread/src/java_Thread4/State.java
@@ -0,0 +1,5 @@
+package java_Thread4;
+
+public class State {
+
+}
---
+++ java_thread/src/java_Thread4/ThState.java
@@ -0,0 +1,19 @@
+package java_Thread4;
+
+public enum ThState {
+	NEW, RUNNABLE, BLOCKED, WATING, TIMED,_WATING, TERMINATED
+}
+
+/*
+	
+	상수들의 클래스처럼 사용할 수 있는 방법 - Enum
+	
+	자바 상수
+	final int num=20;
+	num=30; 오류
+	
+	자바스크립트 상수
+	const num=10;
+	num=30;	오류
+	
+*/
