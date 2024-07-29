aedf38ade39239645fe04c9f247c2c282d5d4bc9
kim1677 <rudwpww@naver.com>
2024-07-29 17:30:32 +0900

    123

--- jstl/src/main/java/controller/TestServlet1.java
+++ jstl/src/main/java/controller/TestServlet1.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class TestServlet1
  */
-@WebServlet("/TestServlet1")
+@WebServlet("/my")
 public class TestServlet1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
