04159c9d67c52409d63a2d2debb03a3e1ce4d30e
kim1677 <rudwpww@naver.com>
2024-08-01 08:58:46 +0900

    123

--- jstl/src/main/java/DAO/BoardDAO.java
+++ jstl/src/main/java/DAO/BoardDAO.java
@@ -1,59 +1,10 @@
 package DAO;
 
+import DTO.BoardDTO;
 
-public class BoardDAO {
-	private int memberId;
-	private String userId;
-	private String userPassword;
-	private String userEmail;
-	private String userName;
-	
-	public BoardDAO() {}
-	public BoardDAO(String userId, String userPassword, 
-			String userEmail, String userName) {
-		this.userId=userId;
-		this.userPassword=userPassword;
-		this.userEmail=userEmail;
-		this.userName=userName;
-	}
-	public BoardDAO(int id, String userId, String userPassword,
-			String userEmail, String userName) {
-		this(userId,userPassword, userEmail, userName);
-		this.memberId=id;
-	}
-	
+public class BoardDAO extends DBConnect{
 
-	public int getMemberId() {
-		return memberId;
+	public void save(BoardDTO board) {
+		
 	}
-	public void setMemberId(int memberId) {
-		this.memberId = memberId;
-	}
-	public String getUserId() {
-		return userId;
-	}
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-	public String getUserPassword() {
-		return userPassword;
-	}
-	public void setUserPassword(String userPassword) {
-		this.userPassword = userPassword;
-	}
-	public String getUserEmail() {
-		return userEmail;
-	}
-	public void setUserEmail(String userEmail) {
-		this.userEmail = userEmail;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	
-	
-
 }
--- jstl/src/main/java/service/BoardWrite.java
+++ jstl/src/main/java/service/BoardWrite.java
@@ -11,22 +11,21 @@ public class BoardWrite implements MainActive {
 	@Override
 	public String action(HttpServletRequest request, HttpServletResponse response) {
 		String method = request.getParameter("method");
-		if(method !=null ) {	// 게시글 작성할 경우
-			String title = request.getParameter("title");
+		if( method != null ) { // 게시글 작성 할경우
+			String title= request.getParameter("title");
 			String content = request.getParameter("content");
-			String writer = (String)request.getSession().getAttribute("content");
+			String writer = (String)request.getSession().getAttribute("user");
 			
 			BoardDTO dto = new BoardDTO(writer, title, content, writer);
 			
 			BoardDAO dao = new BoardDAO();
 			dao.save(dto);
-			try {response.sendRedirect("/board.do");}catch(Exception e) {}
+			try { response.sendRedirect("/board.do"); }catch(Exception e) {}
 			return null;
 			
-		}else {					// 게시글 작성페이지 
+		}else {// 게시글 작성페이지 요청한 경우
 			return "board/boardWrite.jsp";
 		}
 	}
-	
 
 }
