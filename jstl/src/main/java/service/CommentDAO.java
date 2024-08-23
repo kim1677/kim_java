10f9b3ae6828d7bc614e8137d6e23ecccfe03db9
kim1677 <rudwpww@naver.com>
2024-08-23 17:33:00 +0900

    123

--- jstl/src/main/java/service/CommentDAO.java
+++ jstl/src/main/java/service/CommentDAO.java
@@ -46,6 +46,20 @@ public class CommentDAO extends DBConnect{
 		return list;
 	}
 
+	public void delete(int cid) {
+		String sql="select * from comment where board_id=?";
+		
+		try {
+			pt = conn.prepareStatement(sql);
+			pt.setInt(1, cid);
+			rs=pt.executeQuery();
+		}catch(SQLException e) {
+			System.out.println("댓글 삭제실패");
+			e.printStackTrace();
+		}
+		
+		
+	}
 
 
 }
--- jstl/src/main/java/service/CommentDelete.java
+++ jstl/src/main/java/service/CommentDelete.java
@@ -12,13 +12,14 @@ public class CommentDelete implements MainActive {
 	@Override
 	public String action(HttpServletRequest request, HttpServletResponse response) {
 		
-		int bid = Integer.parseInt(request.getParameter("id"));
+		int cid = Integer.parseInt(request.getParameter("id"));
+		int bid = Integer.parseInt(request.getParameter("bid"));
 		CommentDAO dao = new CommentDAO();
-		dao.delete(bid);
+		dao.delete(cid);
 		
 		try {
-			response.sendRedirect("/baord.do");
-		}catch(SQLException e) {
+			response.sendRedirect("/boardView.do?id="+bid);
+		}catch(Exception e) {
 			
 		}
 		
